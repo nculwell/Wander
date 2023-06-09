6988a88c0c1383598895a3668df768a4a51b5ff7
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-09 19:18:09 +0800

    Delete src directory

--- src/lighttpd_start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-if "%CYGWIN_HOME%"=="" set CYGWIN_HOME=C:\cygwin
-"%CYGWIN_HOME%\bin\perl.exe" lighttpd_conf_repl.pl "%CYGWIN_HOME%"
-"%CYGWIN_HOME%\usr\sbin\lighttpd.exe" -f %~dp0\lighttpd.conf
